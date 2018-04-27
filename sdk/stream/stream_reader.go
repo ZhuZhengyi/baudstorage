2f43bd8b9f5ecee731a207ca8d4eb0488abb895f
bjguoweilong <guowL$0917>
2018-04-27 11:30:58 +0800

    init

--- sdk/stream/stream_reader.go
+++ sdk/stream/stream_reader.go
@@ -1,4 +1,26 @@
 package stream
 
+import "github.com/tiglabs/baudstorage/sdk"
+
 type StreamReader struct {
+	inode uint64
+	key ExtentKey
+	start uint64
+	end uint64
+	wraper *sdk.VolGroupWraper
+	vol *sdk.VolGroup
+	data []byte
 }
+
+
+
+func NewStreamReader(inode,start,end uint64,key ExtentKey)(reader *StreamReader){
+	reader=new(StreamReader)
+	reader.inode=inode
+	reader.key=key
+	reader.start=start
+
+	return
+}
+
+
