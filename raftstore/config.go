0363f7eb2967bf0ebd6b752cf78c99470fcc7cbe
Mervin <mofei2816@gmail.com>
2018-05-11 18:11:58 +0800

    Add some comment.

--- raftstore/config.go
+++ raftstore/config.go
@@ -4,16 +4,19 @@ import (
 	"github.com/tiglabs/raft/proto"
 )
 
+// Constants for network port definition.
 const (
 	HeartbeatPort = 9901
 	ReplicatePort = 9902
 )
 
+// Config defined necessary configuration properties for raft store.
 type Config struct {
-	NodeID  uint64 // Identity of this node.
-	WalPath string // Path of WAL(Write after log)
+	NodeID  uint64 // Identity of raft server instance.
+	WalPath string // Path of WAL(Write after Log)
 }
 
+// PartitionConfig defined necessary configuration properties for raft store partition.
 type PartitionConfig struct {
 	ID      uint64
 	Applied uint64
--- raftstore/resolver.go
+++ raftstore/resolver.go
@@ -8,31 +8,43 @@ import (
 	"sync"
 )
 
+// Error definitions.
 var (
 	ErrNoSuchNode        = errors.New("no such node")
 	ErrIllegalAddress    = errors.New("illegal address")
 	ErrUnknownSocketType = errors.New("unknown socket type")
 )
 
+// This private struct defined necessary properties for node address info storage.
 type nodeAddress struct {
 	Heartbeat string
 	Replicate string
 }
 
+// NodeManager defined necessary methods for node address management.
 type NodeManager interface {
+	// AddNode used to add node address information.
 	AddNode(nodeId uint64, addr string)
+
+	// DeleteNode used to delete node address information
+	// of specified node ID from NodeManager if possible.
 	DeleteNode(nodeId uint64)
 }
 
+// NodeResolver defined necessary methods for both node address resolving and management.
+// It extends from SocketResolver and NodeManager.
 type NodeResolver interface {
 	raft.SocketResolver
 	NodeManager
 }
 
+// This is the default parallel-safe implementation of NodeResolver interface.
 type nodeResolver struct {
 	nodeMap sync.Map
 }
 
+// NodeAddress resolve NodeID to net.Addr addresses.
+// This method is necessary for SocketResolver interface implementation.
 func (r *nodeResolver) NodeAddress(nodeID uint64, stype raft.SocketType) (addr string, err error) {
 	val, ok := r.nodeMap.Load(nodeID)
 	if !ok {
@@ -55,6 +67,7 @@ func (r *nodeResolver) NodeAddress(nodeID uint64, stype raft.SocketType) (addr s
 	return
 }
 
+// AddNode adds node address information.
 func (r *nodeResolver) AddNode(nodeId uint64, addr string) {
 	if len(strings.TrimSpace(addr)) != 0 {
 		r.nodeMap.Store(nodeId, &nodeAddress{
@@ -64,20 +77,24 @@ func (r *nodeResolver) AddNode(nodeId uint64, addr string) {
 	}
 }
 
+// DeleteNode deletes node address information of specified node ID from NodeManager if possible.
 func (r *nodeResolver) DeleteNode(nodeId uint64) {
 	r.nodeMap.Delete(nodeId)
 }
 
+// NewNodeResolver returns a new NodeResolver instance for node address management and resolving.
 func NewNodeResolver() NodeResolver {
 	return &nodeResolver{}
 }
 
+// AddNode add node address into specified NodeManger if possible.
 func AddNode(manager NodeManager, nodeId uint64, addr string) {
 	if manager != nil {
 		manager.AddNode(nodeId, addr)
 	}
 }
 
+// DeleteNode delete node address data from specified NodeManager if possible.
 func DeleteNode(manager NodeManager, nodeId uint64) {
 	if manager != nil {
 		manager.DeleteNode(nodeId)
