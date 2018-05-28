9237e31af2922448e968f3f2b521cf18d5d64634
znlstar <>
2018-05-28 16:16:28 +0800

    fix: ino get proxy error

--- metanode/manager_op.go
+++ metanode/manager_op.go
@@ -262,18 +262,23 @@ func (m *metaManager) opMetaInodeGet(conn net.Conn, p *Packet) (err error) {
 	if err = json.Unmarshal(p.Data, req); err != nil {
 		p.PackErrorWithBody(proto.OpErr, nil)
 		m.respondToClient(conn, p)
+		err = errors.Errorf("[opMetaInodeGet]: %s", err.Error())
 		return
 	}
+	log.LogDebugf("[opMetaInodeGet] Get request: %v", req)
 	mp, err := m.getPartition(req.PartitionID)
 	if err != nil {
 		p.PackErrorWithBody(proto.OpNotExistErr, nil)
 		m.respondToClient(conn, p)
+		err = errors.Errorf("[opMetaInodeGet]%s", err.Error())
 		return
 	}
-	if m.serveProxy(conn, mp, p) {
+	if !m.serveProxy(conn, mp, p) {
 		return
 	}
-	err = mp.InodeGet(req, p)
+	if err = mp.InodeGet(req, p); err != nil {
+		err = errors.Errorf("[opMetaInodeGet]%s", err.Error())
+	}
 	m.respondToClient(conn, p)
 	return
 }
