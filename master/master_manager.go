1573bd40ec05a4071a1007db64745a733604d9ed
zhuhyc <zzhniy.163.niy@163.com>
2018-05-29 12:10:47 +0800

    add debug log

--- master/master_manager.go
+++ master/master_manager.go
@@ -12,7 +12,7 @@ type LeaderInfo struct {
 
 func (m *Master) handleLeaderChange(leader uint64) {
 	m.leaderInfo.addr = AddrDatabase[leader]
-	log.LogDebugf("leader is changed:%v",m.leaderInfo.addr)
+	log.LogDebugf("leader is changed:%v", m.leaderInfo.addr)
 	m.cluster.checkDataNodeHeartbeat()
 	m.cluster.checkDataNodeHeartbeat()
 }
