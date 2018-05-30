47d287408ceb7c4df2a70cfbdc1e83fe5b3acd33
bjguoweilong <guowL$0917>
2018-05-30 12:53:53 +0800

    fix log level bug

--- sdk/stream/stream_writer_test.go
+++ sdk/stream/stream_writer_test.go
@@ -87,7 +87,7 @@ func initClient(t *testing.T) (client *ExtentClient) {
 		fmt.Println(http.ListenAndServe("localhost:6060", nil))
 	}()
 	var err error
-	client, err = NewExtentClient("stream_write_test", "127.0.0.1:7778", saveExtentKey, updateKey)
+	client, err = NewExtentClient("it3", "10.196.31.173:80", saveExtentKey, updateKey)
 	if err != nil {
 		OccoursErr(fmt.Errorf("init client err[%v]", err.Error()), t)
 	}
