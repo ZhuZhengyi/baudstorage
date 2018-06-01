13c8618b7abc9376b89b5f45309f6f09c37ba292
Shuoran Liu <sjzlsr@126.com>
2018-06-01 11:34:35 +0800

    Remove ump perf from fuse

--- client/fs/dir.go
+++ client/fs/dir.go
@@ -8,7 +8,6 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/tiglabs/baudstorage/util/log"
-	"github.com/tiglabs/baudstorage/util/ump"
 )
 
 type Dir struct {
@@ -41,13 +40,9 @@ func NewDir(s *Super, p *Dir) *Dir {
 }
 
 func (d *Dir) Attr(ctx context.Context, a *fuse.Attr) error {
-	var err error
 	log.LogDebugf("Dir Attr: ino(%v)", d.inode.ino)
-	umpKey := d.super.umpKey("Attr")
-	tpObject := ump.BeforeTP(umpKey)
-	defer ump.AfterTP(tpObject, err)
 
-	err = d.super.InodeGet(d.inode.ino, &d.inode)
+	err := d.super.InodeGet(d.inode.ino, &d.inode)
 	if err != nil {
 		log.LogErrorf("Dir Attr: ino(%v) err(%v)", d.inode.ino, err.Error())
 		return ParseError(err)
@@ -57,11 +52,7 @@ func (d *Dir) Attr(ctx context.Context, a *fuse.Attr) error {
 }
 
 func (d *Dir) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.CreateResponse) (fs.Node, fs.Handle, error) {
-	var err error
 	log.LogDebugf("Dir Create: ino(%v) name(%v)", d.inode.ino, req.Name)
-	umpKey := d.super.umpKey("Create")
-	tpObject := ump.BeforeTP(umpKey)
-	defer ump.AfterTP(tpObject, err)
 
 	info, err := d.super.mw.Create_ll(d.inode.ino, req.Name, ModeRegular)
 	if err != nil {
@@ -81,11 +72,7 @@ func (d *Dir) Forget() {
 }
 
 func (d *Dir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error) {
-	var err error
 	log.LogDebugf("Dir Mkdir: ino(%v) name(%v)", d.inode.ino, req.Name)
-	umpKey := d.super.umpKey("Mkdir")
-	tpObject := ump.BeforeTP(umpKey)
-	defer ump.AfterTP(tpObject, err)
 
 	info, err := d.super.mw.Create_ll(d.inode.ino, req.Name, ModeDir)
 	if err != nil {
@@ -99,11 +86,7 @@ func (d *Dir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error
 }
 
 func (d *Dir) Remove(ctx context.Context, req *fuse.RemoveRequest) error {
-	var err error
 	log.LogDebugf("Dir Remove: ino(%v) name(%v)", d.inode.ino, req.Name)
-	umpKey := d.super.umpKey("Mkdir")
-	tpObject := ump.BeforeTP(umpKey)
-	defer ump.AfterTP(tpObject, err)
 
 	extents, err := d.super.mw.Delete_ll(d.inode.ino, req.Name)
 	if err != nil {
@@ -123,11 +106,7 @@ func (d *Dir) Fsync(ctx context.Context, req *fuse.FsyncRequest) error {
 }
 
 func (d *Dir) Lookup(ctx context.Context, req *fuse.LookupRequest, resp *fuse.LookupResponse) (fs.Node, error) {
-	var err error
 	log.LogDebugf("Dir Lookup: parent(%v) name(%v)", d.inode.ino, req.Name)
-	umpKey := d.super.umpKey("Lookup")
-	tpObject := ump.BeforeTP(umpKey)
-	defer ump.AfterTP(tpObject, err)
 
 	ino, mode, err := d.super.mw.Lookup_ll(d.inode.ino, req.Name)
 	if err != nil {
@@ -159,11 +138,7 @@ func (d *Dir) Lookup(ctx context.Context, req *fuse.LookupRequest, resp *fuse.Lo
 }
 
 func (d *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
-	var err error
 	log.LogDebugf("Dir Readdir: ino(%v)", d.inode.ino)
-	umpKey := d.super.umpKey("Readdir")
-	tpObject := ump.BeforeTP(umpKey)
-	defer ump.AfterTP(tpObject, err)
 
 	dirents := make([]fuse.Dirent, 0)
 	children, err := d.super.mw.ReadDir_ll(d.inode.ino)
@@ -189,13 +164,9 @@ func (d *Dir) Rename(ctx context.Context, req *fuse.RenameRequest, newDir fs.Nod
 		return fuse.ENOTSUP
 	}
 
-	var err error
 	log.LogDebugf("Dir Rename: srcIno(%v) oldName(%v) dstIno(%v) newName(%v)", d.inode.ino, req.OldName, dstDir.inode.ino, req.NewName)
-	umpKey := d.super.umpKey("Readdir")
-	tpObject := ump.BeforeTP(umpKey)
-	defer ump.AfterTP(tpObject, err)
 
-	err = d.super.mw.Rename_ll(d.inode.ino, req.OldName, dstDir.inode.ino, req.NewName)
+	err := d.super.mw.Rename_ll(d.inode.ino, req.OldName, dstDir.inode.ino, req.NewName)
 	if err != nil {
 		log.LogErrorf("Dir Rename: srcIno(%v) oldName(%v) dstIno(%v) newName(%v) err(%v)", d.inode.ino, req.OldName, dstDir.inode.ino, req.NewName, err.Error())
 		return ParseError(err)
