6117babb8dca1d52c8c811e087d56972b341faf2
Mervin <mofei2816@gmail.com>
2018-06-05 12:22:06 +0800

    fix field type

--- metanode/server.go
+++ metanode/server.go
@@ -13,7 +13,7 @@ import (
 func (m *MetaNode) startServer() (err error) {
 	// Init and start server.
 	m.httpStopC = make(chan uint8)
-	ln, err := net.Listen("tcp", ":"+strconv.Itoa(m.listen))
+	ln, err := net.Listen("tcp", ":"+m.listen)
 	if err != nil {
 		return
 	}
